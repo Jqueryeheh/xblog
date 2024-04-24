1a8c2655bfceb554756981ada7edc3175cf35736
Jqueryeheh <77944224+Jqueryeheh@users.noreply.github.com>
2024-04-25 01:56:31 +0300

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,4 +1,10 @@
 import express from 'express'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 let dir = "file:///opt/render/project/src/";
@@ -7,7 +13,7 @@ app.use(express.static(dir))
 let port = 3000;
 
 app.get("/",(req,res)=>{
-	res.sendFile(dir+"index.html");
+	res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 app.listen(port,()=>{
