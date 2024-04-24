2d698246347836458c06a83a9b63a6d21edc394c
Jqueryeheh <77944224+Jqueryeheh@users.noreply.github.com>
2024-04-25 01:30:40 +0300

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,15 +1,20 @@
 import express from 'express'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-let dir =__dirname
+let dir = path.join(__dirname, 'public');
 
 app.use(express.static(dir))
 
 let port = 3000;
 
 app.get("/",(req,res)=>{
-	res.sendFile(dir+"/index.html");
+	res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 app.listen(port,()=>{
