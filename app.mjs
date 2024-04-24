fb075638aa6e31bd1a3d2f179853a322b04c30de
Jqueryeheh <77944224+Jqueryeheh@users.noreply.github.com>
2024-04-25 01:15:45 +0300

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -2,14 +2,14 @@ import express from 'express'
 
 const app = express();
 
-let dir = '/data/data/com.termux/files/home/J/xblog'
+let dir ="" 
 
 app.use(express.static(dir))
 
 let port = 3000;
 
 app.get("/",(req,res)=>{
-	res.sendFile("/index.html");
+	res.sendFile("./index.html");
 })
 
 app.listen(port,()=>{
