ac607c3e0f5e38d7760ed41e27b98704d5cab471
Jqueryeheh <77944224+Jqueryeheh@users.noreply.github.com>
2024-04-25 12:17:19 +0300

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,5 +1,7 @@
 import express from 'express'
 
+ import { exec } from 'child_process'
+
 const app = express();
 
 let dir = "/opt/render/project/src/";
@@ -9,8 +11,14 @@ let port = 3000;
 
 app.get("/",(req,res)=>{
 	res.sendFile(dir+"index.html");
+	
 })
 
 app.listen(port,()=>{
 	console.log("started");
+	exec('pwd', (error, stdout, stderr) => {
+     if (error) {
+       return;
+     }
+	})
 })
